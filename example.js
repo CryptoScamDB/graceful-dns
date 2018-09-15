e71b49f9445ec36409b2b170cd12f49c49d09c05
Luit Hollander <MrLuit@users.noreply.github.com>
2018-09-15 20:49:04 +0200

    Move to cryptoscamdb org

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-const dns = require('./index');
+const dns = require('./index');  // const dns = require('@cryptoscamdb/graceful-dns');
 
 (async () => {
 	const nameservers = await dns.getNS("google.com");
