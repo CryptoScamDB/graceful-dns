da6b62fe478755cb2af94702410db2c742b0ccfa
Mrtenz <maarten@zuidhoorn.com>
2018-09-30 13:47:01 +0200

    Update README.md

--- example.js
+++
@@ -1,6 +0,0 @@
-const dns = require('./index'); // const dns = require('@cryptoscamdb/graceful-dns');
-
-(async () => {
-    const nameservers = await dns.getNS('google.com');
-    console.log(nameservers);
-})();
