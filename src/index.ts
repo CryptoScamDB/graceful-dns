c5c4b87ce330309f78c4a4c4dd53954f6c52951a
Mrtenz <maarten@zuidhoorn.com>
2018-09-30 13:26:37 +0200

    Refactor index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,70 @@
-const dns = require('dns');
-const url = require('url');
+import * as dns from 'dns';
+import * as url from 'url';
 
-module.exports.getServers = dns.getServers;
+/**
+ * Get the IP address of a hostname.
+ * @param hostname
+ */
+export const getIP = (hostname: string): Promise<string | undefined> => {
+  return new Promise(resolve => {
+    dns.lookup(url.parse(hostname).hostname || hostname, (error, address) => {
+      if (error) {
+        return resolve(undefined);
+      }
 
-module.exports.setServers = dns.setServers;
+      resolve(address);
+    });
+  });
+};
 
-module.exports.getIP = (hostname) => {
-	return new Promise(resolve => {
-		dns.lookup(url.parse(hostname).hostname || hostname, (err,address) => {
-			if(err) resolve(undefined);
-			else resolve(address);
-		});
-	});
-}
+/**
+ * Get all DNS records associated with a hostname.
+ * @param hostname
+ */
+export const getDNS = (hostname: string): Promise<dns.AnyRecord[] | undefined> => {
+  return new Promise(resolve => {
+    dns.resolveAny(url.parse(hostname).hostname || hostname, (error, records) => {
+      if (error) {
+        return resolve(undefined);
+      }
 
-module.exports.getDNS = (hostname) => {
-	return new Promise(resolve => {
-		dns.resolveAny(url.parse(hostname).hostname || hostname, (err,records) => {
-			if(err) resolve(undefined);
-			else resolve(records);
-		});
-	});
-}
+      resolve(records);
+    });
+  });
+};
 
-module.exports.getNS = (hostname) => {
-	return new Promise(resolve => {
-		dns.resolveNs(url.parse(hostname).hostname || hostname, (err,addresses) => {
-			if(err) resolve(undefined);
-			else resolve(addresses);
-		});
-	});
-}
+/**
+ * Get the name servers associated with a hostname.
+ * @param hostname
+ */
+export const getNS = (hostname: string): Promise<string[] | undefined> => {
+  return new Promise(resolve => {
+    dns.resolveNs(url.parse(hostname).hostname || hostname, (error, addresses) => {
+      if (error) {
+        return resolve(undefined);
+      }
 
-module.exports.reverseDNS = (ip) => {
-	return new Promise(resolve => {
-		dns.reverse(ip, (err,hostnames) => {
-			if(err) resolve(undefined);
-			else resolve(hostnames);
-		});
-	});
-}
+      resolve(addresses);
+    });
+  });
+};
+
+/**
+ * Do a reverse DNS lookup for an IP address.
+ * @param ip
+ */
+export const reverseDNS = (ip: string): Promise<string[] | undefined> => {
+  return new Promise(resolve => {
+    dns.reverse(ip, (error, hostnames) => {
+      if (error) {
+        return resolve(undefined);
+      }
+
+      resolve(hostnames);
+    });
+  });
+};
+
+export const getServers = dns.getServers;
+
+export const setServers = dns.setServers;
